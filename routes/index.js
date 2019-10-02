c354e3530c1e470dc781cf3401864fce1d00284a
Vivek Molkar <vivekmolkar@gmail.com>
2019-10-02 20:34:54 +0530

    Completed home page layout, Completed routes to generate QR from text

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+let generateQR = require('../controllers/qr-generator').generateCode;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'QRCode Generator App' });
+router.get('/', async (req, res, next) => {
+  let image = await generateQR('Enter text here to share.');
+  res.render('index', { title: 'QRCode Generator App', image: image });
+});
+
+router.post('/', async (req, res, next) => {
+  let text = (req.body.inputText || '').trim();
+  let image = await generateQR(text);
+  res.render('index', { title: 'QRCode Generator App', image: image });
 });
 
 module.exports = router;
