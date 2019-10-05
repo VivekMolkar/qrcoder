bfdba15111fa1bd0070075b5141877b91e8cd7c8
Vivek Molkar <vivekmolkar@gmail.com>
2019-10-05 14:35:42 +0530

    Versioning and display on footer

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 let generateQR = require('../controllers/qr-generator').generateCode;
+let config = require('../config');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   let image = await generateQR('Enter text here to share.');
-  res.render('index', { title: 'QRCode Generator App', image: image });
+  res.render('index', {
+    title: 'QRCode Generator App',
+    image: image,
+    year: new Date().getFullYear(),
+    version: config.version
+  });
 });
 
 router.post('/', async (req, res, next) => {
@@ -14,7 +20,12 @@ router.post('/', async (req, res, next) => {
   let fgColor = req.body.fgColor;
   let bgColor = req.body.bgColor;
   let image = await generateQR(text, fgColor, bgColor);
-  res.render('index', { title: 'QRCode Generator App', image: image });
+  res.render('index', {
+    title: 'QRCode Generator App',
+    image: image,
+    year: new Date().getFullYear(),
+    version: config.version
+  });
 });
 
 module.exports = router;
